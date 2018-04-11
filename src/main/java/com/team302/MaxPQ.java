9c2b1b351a25deccc02a760934e624e751073468
lzmkunkun <liang.zhi@husky.neu.edu>
2018-04-11 02:51:43 -0400

    maxpq

--- src/main/java/com/team302/MaxPQ.java
+++ src/main/java/com/team302/MaxPQ.java
@@ -1,4 +1,23 @@
 package com.team302;
 
+import java.util.ArrayList;
+
 public class MaxPQ<Sudoku> {
+
+    public ArrayList<Sudoku> pq = new ArrayList<Sudoku>();
+    public int N;
+
+    public MaxPQ() {
+    }
+
+    public boolean isEmpty(){
+        return N==0;
+    }
+    public void insert(Sudoku sudoku){
+
+    }
+
+    public Sudoku delMax(){}
+
+    private void swim(){}
 }
