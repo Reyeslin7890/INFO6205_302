7d4829171980f64171304777b684f8a4f32c75e9
Jin Lin <lin.jin2@husky.neu.edu>
2018-04-11 00:45:16 -0400

    sudoku class

---
+++ src/main/java/com/team302/Sudoku.java
@@ -0,0 +1,10 @@
+package com.team302;
+
+public class Sudoku {
+    public int[] code;
+    public static int unused;
+    public static int[] index = new int[9];
+    public Sudoku() {
+        code = new int[unused];
+    }
+}
